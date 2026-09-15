142fd971f91c1f0848515b85c92a6bfdc10bb836
edunovaProgramiranje <84087934+edunovaProgramiranje@users.noreply.github.com>
2026-09-15 18:35:40 +0200

    Provjera rada

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Dobo došli na AUKCIJE 🙂</h1>
+          <h1>Dobo došli na AUKCIJE 🙂 BRAVO</h1>
           <p>
          Pažljivo pročitajte Uvjete korištenja kako ne bi došlo do neželjenih komplikacija!!!
           </p>

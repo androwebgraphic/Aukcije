fc1fa711a504e663aa5971457cbff8d6c502fb41
edunovaProgramiranje <84087934+edunovaProgramiranje@users.noreply.github.com>
2026-09-22 17:34:14 +0200

    Sitnice

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,18 @@
+import { APP_NAME } from "../constants";
 
 
 
 export default function Home() {
   return (
   <>
-  Pocetna  sadrzaj dojde  kasnijer
+ <h1>Dobro došli na {APP_NAME}</h1>
+          <p className='warn'><strong>Pažljivo pročitajte Uvjete korištenja i potvrdite <cite>"Slažem se"</cite> kako ne bi došlo do neželjenih komplikacija</strong></p>
+          <h2>O nama </h2>
+          <p>Ova aplikacija {APP_NAME} je namijenjena kolekcionarima  koji nešto žele kupiti ili prodati</p>
+          <p className='warn'>Isključivo je zabranjen bilo kakav oblik vrijeđanja na rasnoj, vjerskoj i nacionalnoj osnovi<br></br>
+            Svatko tko se ne bude pridžavao "Uvjeta korištenja" bit će mu blokiran a potom i uklonjen račun s Aukcija.
+          </p>
+          <p>Sretno svima 😀</p>
   
   </>
 )

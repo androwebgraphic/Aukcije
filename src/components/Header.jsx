87b0af1a2c81bea33e07db4b0f8d61e6d02c85d0
androwebgraphic <andreassklizovic@gmail.com>
2026-09-24 13:21:47 +0200

    css i header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,18 +3,19 @@ import NavMain from "./NavMain";
 
 function Header() {
   return(
-  <>
+    <>
+      
       <header>
+      
           <img className="logo" src="src/img/aukcijelogo.svg" />
         {/* <h1>Aukcije</h1> */}
-      
+     
        <NavMain></NavMain>
-
 
 
   </header>
   
-  
+
   
   
   </>
